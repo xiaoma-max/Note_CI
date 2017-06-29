309baddfa10dae5aad841b8d08b237ad074384cb
xiaoma-max <xiaoma_max@hotmail.com>
2017-06-29 11:18:10 +1000

    Project init

---
+++ Pods/Target Support Files/Pods-SimpleNoteTakingAppTests/Pods-SimpleNoteTakingAppTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SimpleNoteTakingAppTests : NSObject
+@end
+@implementation PodsDummy_Pods_SimpleNoteTakingAppTests
+@end
---
+++ Pods/Target Support Files/Pods-SimpleNoteTakingAppTests/Pods-SimpleNoteTakingAppTests-umbrella.h
@@ -0,0 +1,8 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+
+FOUNDATION_EXPORT double Pods_SimpleNoteTakingAppTestsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_SimpleNoteTakingAppTestsVersionString[];
+
